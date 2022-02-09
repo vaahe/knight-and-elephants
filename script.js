d405f3502ccbca584b43acb0c58d86711f678478
Vahe Barseghyan <94294219+vaahe@users.noreply.github.com>
2022-02-09 08:15:45 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,80 @@
-let canvas = document.getElementById('canvas');
-
+const container = document.getElementById('container');
 const startButton = document.getElementById('startButton');
 const modal = document.getElementById('modal');
+const select = document.getElementById('size');
+let currNode;
 
-startButton.addEventListener('click', function() {
-    modal.style.display = "none";
-    selector();
-});
+function createElephant(x, y) {
+    let nodes = container.childNodes;
 
-function createField(x, y) {
-    let gridItems = [];
-    let canvasItem;
-    let rand;
-    for (let i = 0; i < x * y; i++) {
-        canvasItem = document.createElement('div');
-        canvasItem.classList.add('canvas-item');
-        canvas.appendChild(canvasItem);
-        gridItems.push(i);
-    }
+    for (let i = 0; i < x * y / 5; i++) {
+        let rand = Math.floor(Math.random() * (x * y));
+        let elephantImg = document.createElement('img');
 
-    /*    let nodes = canvas.childNodes;
-       let elephantImg = document.createElement('img');
-       elephantImg.src = "img/elephant.png";
-       elephantImg.width = "48";
-       elephantImg.heigh = "48";
-       nodes[rand].appendChild(elephantImg); 
+        elephantImg.src = "img/elephant.png";
+        elephantImg.width = '48';
+        elephantImg.height = '48';
+        nodes[rand].appendChild(elephantImg);
+        console.log(rand);
 
-    rand = Math.floor(Math.random() * (5 * 5));
-    let knightImg = document.createElement('img');
-    knightImg.src = "img/knight.png";
-    knightImg.width = "48";
-    knightImg.height = "48";
-    nodes[rand].appendChild(knightImg); 
-    */
-    for (let i = 0; i < 5; i++) {
-        var nums = [...gridItems],
-            ranNums = [],
-            x = nums.length,
-            j = 0;
-        while (x--) {
-            j = Math.floor(Math.random() * (x + 1));
-            ranNums.push(nums[j]);
-            nums.splice(j, 1);
-            console.log(j);
+        if (select.selectedIndex == 2) {
+            elephantImg.width = '33';
+            elephantImg.height = '33';
         }
     }
+}
 
+function createKnight(x, y) {
+    let nodes = container.childNodes;
+    let rand = Math.floor(Math.random() * (x * y));
+    let knightImg = document.createElement('img');
+
+    knightImg.src = "img/knight.png";
+    knightImg.width = '48';
+    knightImg.height = '48';
+    currNode = nodes[rand].appendChild(knightImg);
+
+    if (select.selectedIndex == 2) {
+        knightImg.width = '33';
+        knightImg.height = '33';
+    }
+
+   
+}
+
+function createField(x, y) {
+    for (let i = 0; i < x * y; i++) {
+        let containerItem = document.createElement('div');
+        containerItem.classList.add('container-item');
+        container.appendChild(containerItem);
+    }
+
+    createElephant(x, y);
+    createKnight(x, y);
 }
 
 function selector() {
-    const select = document.getElementById('size');
     if (select.selectedIndex == 0) {
-        canvas.style.grid = "repeat(" + 5 + ", 50px) / repeat(" + 5 + ", 50px)";
+        container.style.grid = "repeat(" + 5 + ", 50px) / repeat(" + 5 + ", 50px)";
         createField(5, 5);
     } else if (select.selectedIndex == 1) {
-        canvas.style.grid = "repeat(" + 10 + ", 50px) / repeat(" + 10 + ", 50px)";
+        container.style.grid = "repeat(" + 10 + ", 50px) / repeat(" + 10 + ", 50px)";
         createField(10, 10);
     } else if (select.selectedIndex == 2) {
-        canvas.style.grid = "repeat(" + 15 + ", 35px) / repeat(" + 15 + ", 35px)";
+        container.style.grid = "repeat(" + 15 + ", 35px) / repeat(" + 15 + ", 35px)";
         createField(15, 15);
     }
 }
+
+function render() {
+    currNode.addEventListener('keypress', (e) => {
+        if (e.key == 'w') {
+            alert(1);
+        }
+    })
+}
+
+startButton.addEventListener('click', () => {
+    modal.style.display = "none";
+    selector();
+});
